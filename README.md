5eef025163a94089c310cf20f01b54e4b8c3bac2
ktetsuo <ktetsuo@users.noreply.github.com>
2018-05-15 22:17:59 +0900

    ソースに書いていた環境設定方法をREADME.mdに移動

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # YHForever
 YH Forever
+
+
+## Arduino環境設定
+* このスケッチは [Nefry BT](https://dotstud.io/docs/nefrybt/) のライブラリを使用しています。
+* Preference>環境設定>追加のボードマネージャのURL にhttp://nefry.studio/package_nefrybt_index.json を入力してから、ボードマネージャから Nefry(ESP32) をインストールしてください。
+* ボードは Nefry(ESP32)Module > ESP32 DevModule を選択してください。
--- YHForever.ino
+++ YHForever.ino
@@ -1,9 +1,3 @@
-// このスケッチは Nefry BT のライブラリを使用しています。
-// メニューのPreferenceから、環境設定のダイアログを開いて
-// 追加のボードマネージャのURLに http://nefry.studio/package_nefrybt_index.json
-// を入力してから、ボードマネージャから Nefry(ESP32) をインストールしてください。
-// ボードは Nefry(ESP32)Module > ESP32 DevModule を選択してください。
-
 #include <Nefry.h>
 #include "remocon.h"
 
@@ -12,8 +6,6 @@
 // SPI CS:25, SCK:18, MISO:19, MOSI:23
 // AD A3:39, A4:32, A5:33, A6: 34, A7:35, A12:2,
 //    A13:15, A14:13, A15:12, A16:14, A17:27, A18:25, A19:26
-
-const char type[] PROGMEM = "text/html";
 
 void setup() {
   Nefry.setProgramName("YHRemocon");//プログラム名登録
